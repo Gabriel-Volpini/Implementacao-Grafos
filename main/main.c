2cc6117d914260c56c6bb9fc3d3650db2ad733a0
Volpini <bielpini1@gmail.com>
2021-03-03 19:58:35 -0300

    Correção de leitura de dados

--- main/main.c
+++ main/main.c
@@ -7,39 +7,39 @@
 
 //padrão  (entrada,saida)
 
-typedef struct grafo{
-    char vert;
+typedef struct grafo {
+    int vert;
     int prio;
     struct grafo *prox,*sucessor;
 } grafo;
 
-void organiza(char entradaAtual,char saidaAtual,grafo *vert){
-    while(vert->vert != entradaAtual){
-        if(vert->prox != NULL)
-            vert = vert->prox;
+void organiza(int entradaAtual, int saidaAtual, grafo *vert){
+    while(vert -> vert != entradaAtual){
+        if(vert -> prox != NULL)
+            vert = vert -> prox;
         else{
-            vert->prox = malloc(sizeof(grafo));
-            vert->prox->vert = entradaAtual;
-            vert->prox->prox = NULL;
-            vert->prox->sucessor = NULL;
+            vert -> prox = malloc(sizeof(grafo));
+            vert -> prox -> vert = entradaAtual;
+            vert -> prox -> prox = NULL;
+            vert -> prox -> sucessor = NULL;
         }
     }
-    while(vert->sucessor != NULL)
-        vert = vert->sucessor;
-    vert->sucessor = malloc(sizeof(grafo));
-    vert->sucessor->vert = saidaAtual;
-    vert->sucessor->prox = NULL;
-    vert->sucessor->sucessor = NULL;
+    while(vert -> sucessor != NULL)
+        vert = vert -> sucessor;
+    vert -> sucessor = malloc(sizeof(grafo));
+    vert -> sucessor -> vert = saidaAtual;
+    vert -> sucessor -> prox = NULL;
+    vert -> sucessor -> sucessor = NULL;
 }
 
 void imprime(grafo *vert){
     grafo *suc;
     vert = vert->prox;
     while(vert != NULL){
-        printf("\nvertice:%c\n",vert->vert);
+        printf("\nvertice:%d\n",vert->vert);
         suc = vert->sucessor;
         while(suc != NULL){
-            printf("sucessor:%c\t",suc->vert);
+            printf("sucessor:%d\t",suc->vert);
             suc = suc->sucessor;
         }
         vert = vert->prox;
@@ -51,25 +51,22 @@ int main(void) {
     if( !arquivo ) perror(CAMINHO_ARQUIVO), exit(1);
 
     char itemGrafo[TAMANHO_GRAFO];
-    char entradaAtual,saidaAtual;
+    int entrada = 0, saida = 0;
+
     grafo *vert;
     vert = malloc(sizeof(grafo));
-    vert->vert = ' ';
-    vert->prox = NULL;
-    vert->sucessor = NULL;
-    vert->prio = 0;
+    vert -> vert = ' ';
+    vert -> prox = NULL;
+    vert -> sucessor = NULL;
+    vert -> prio = 0;
 
-    while (fgets(itemGrafo, TAMANHO_GRAFO, arquivo) != NULL){
-
-        //Separando os valores de entrada e saída das outros chars do arquvo
-        entradaAtual = itemGrafo[1], saidaAtual = itemGrafo[3];
-
-        printf("%c %c\n",entradaAtual,saidaAtual);
-
-        organiza(entradaAtual,saidaAtual, vert);
+    while (!feof (arquivo)){
+        fscanf(arquivo, "(%d,%d),", &entrada, &saida);
+        organiza(entrada, saida, vert);
     }
     imprime(vert);
-    printf("Fechando arquivo..");
+    
     fclose(arquivo);
+    printf("\nFechando arquivo..");
     return 0;
 }
