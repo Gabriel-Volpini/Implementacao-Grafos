73841bbf9d30e7b020154c06d1a65db8199dd31e
Volpini <bielpini1@gmail.com>
2021-03-03 21:21:45 -0300

    Correção liberação memória

--- main/main.c
+++ main/main.c
@@ -1,71 +1,128 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdbool.h> 
+#include <string.h>
 
 #define CAMINHO_ARQUIVO "data.txt"
-#define TAMANHO_GRAFO 7 //"(entrada,saida)"
-
-//padrão  (entrada,saida)
+//padrão  (entrada,saida) ou (entrada,saida,peso)
 
 typedef struct grafo {
     int vert;
-    int prio;
-    struct grafo *prox,*sucessor;
+    int peso;
+    struct grafo *proximo, *sucessor;
 } grafo;
 
-void organiza(int entradaAtual, int saidaAtual, grafo *vert){
+void organiza(bool ehDirecional, int entradaAtual, int saidaAtual, int peso, grafo *vert){
     while(vert -> vert != entradaAtual){
-        if(vert -> prox != NULL)
-            vert = vert -> prox;
+        if(vert -> proximo != NULL)
+            vert = vert -> proximo;
         else{
-            vert -> prox = malloc(sizeof(grafo));
-            vert -> prox -> vert = entradaAtual;
-            vert -> prox -> prox = NULL;
-            vert -> prox -> sucessor = NULL;
+            vert -> proximo = malloc(sizeof(grafo));
+            vert -> proximo -> vert = entradaAtual;
+            vert -> proximo -> proximo = NULL;
+            vert -> proximo -> sucessor = NULL;
+            vert -> proximo -> peso = peso;
         }
     }
     while(vert -> sucessor != NULL)
         vert = vert -> sucessor;
     vert -> sucessor = malloc(sizeof(grafo));
     vert -> sucessor -> vert = saidaAtual;
-    vert -> sucessor -> prox = NULL;
+    vert -> sucessor -> proximo = NULL;
     vert -> sucessor -> sucessor = NULL;
+    vert -> sucessor -> peso = peso;
+
 }
 
 void imprime(grafo *vert){
     grafo *suc;
-    vert = vert->prox;
+    vert = vert -> proximo;
     while(vert != NULL){
-        printf("\nvertice:%d\n",vert->vert);
-        suc = vert->sucessor;
+        printf("\nvertice:%d\n",vert -> vert);
+        suc = vert -> sucessor;
         while(suc != NULL){
-            printf("sucessor:%d\t",suc->vert);
+            printf("sucessor:%d\t",suc -> vert);
             suc = suc->sucessor;
         }
-        vert = vert->prox;
+        vert = vert -> proximo;
     }
+}
+
+bool menuGetPeso () {
+    int resposta;
+    printf("O arquivo de texto possui peso?\n1- Sim\n2- Nao\n"); 
+    scanf("%d", &resposta);
+
+    if(resposta == 1) return true;
+    else if(resposta == 2) return false;
+    else {
+        printf("Valor invalido, tente novamente\n\n");
+        return menuGetPeso();
+    }
+}
+
+void liberaMemoria (grafo *vert) {
+    grafo *guardado, *prox, *suc;
+
+    suc = vert -> proximo -> sucessor;
+    prox = vert -> proximo;
+
+    while(prox -> proximo != NULL){
+
+        while(suc -> sucessor != NULL){
+            guardado = suc;
+            suc = suc -> sucessor;
+            free(guardado);
+        }
+        guardado = prox;
+        prox = prox -> proximo;
+        suc = prox -> sucessor;
+        free(guardado);
+    }
+
+    return;
 }
 
 int main(void) {
     FILE *arquivo = fopen(CAMINHO_ARQUIVO, "r" );
     if( !arquivo ) perror(CAMINHO_ARQUIVO), exit(1);
 
-    char itemGrafo[TAMANHO_GRAFO];
-    int entrada = 0, saida = 0;
+    int entrada = 0, saida = 0, peso = 0;
+    bool ehDirecional;
 
     grafo *vert;
     vert = malloc(sizeof(grafo));
     vert -> vert = ' ';
-    vert -> prox = NULL;
+    vert -> proximo = NULL;
     vert -> sucessor = NULL;
-    vert -> prio = 0;
-
-    while (!feof (arquivo)){
-        fscanf(arquivo, "(%d,%d),", &entrada, &saida);
-        organiza(entrada, saida, vert);
-    }
-    imprime(vert);
+    vert -> peso = 0;
     
+    char initialChar;
+    fscanf(arquivo, " %c", &initialChar);
+    rewind(arquivo); //resetando cursor dentro do arquivo
+
+    if(initialChar == '{') ehDirecional = true;
+    else ehDirecional = false;
+
+    bool possuiPeso = menuGetPeso();
+            
+    if(possuiPeso){
+        while (!feof (arquivo)){
+            fscanf(arquivo, "(%d,%d,%d),", &entrada, &saida, &peso);
+            organiza(ehDirecional, entrada, saida, peso, vert);
+        }
+    } else {
+        while (!feof (arquivo)){
+            fscanf(arquivo, "(%d,%d),", &entrada, &saida);
+            organiza(ehDirecional, entrada, saida, peso, vert);
+        }
+    }
+
+    imprime(vert);
+
+    liberaMemoria(vert);
+
     fclose(arquivo);
     printf("\nFechando arquivo..");
     return 0;
